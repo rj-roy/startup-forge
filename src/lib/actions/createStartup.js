b89b23b4735f41ede25c898e5437548148fed838
rj-roy <dpjdeveloper.me@gmainl.com>
2026-06-20 19:17:18 +0600

    Thats how startup setup is finished

--- src/lib/actions/createStartup.js
+++ src/lib/actions/createStartup.js
@@ -2,11 +2,19 @@
 import { revalidatePath } from "next/cache";
 import { serverMutation } from "../core/server";
 import { getUserSession } from "../core/session";
+import { getStartupByFounderId } from "../api/getData";
 
 export const createStartup = async (data) => {
+    
     const session = await getUserSession();
     if (!session?.user?.id) {
         throw new Error("Unauthorized");
+    };
+
+    const isAlreadyCreated = await getStartupByFounderId(session?.user?.id);
+    if (isAlreadyCreated) {
+        revalidatePath('/forbidden');
+        throw new Error("You have already created a startup");
     };
 
     const res = await serverMutation('/api/startup/create', data, 'POST');
